f9a3dd3fffbd69428a0ec78ea7c24fde7af42d13
3bdelrahman-ahmed <abdohagag854@gmail.com>
2023-12-28 02:29:11 +0200

    intial_project_2

---
+++ lib/features/on_boarding_screen/presentaion/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        width: 100,
+        height: 100,
+        color: Colors.amber,
+      ),
+    );
+  }
+}
---
+++ lib/src/root.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:spa7o_ta7adii/features/on_boarding_screen/presentaion/home_screen.dart';
+
+
+class RootApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
