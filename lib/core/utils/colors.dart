335b9eb5b14cabca4c2426848566beb5a82b43fd
3bdelrahman-ahmed <abdohagag854@gmail.com>
2024-08-16 16:39:21 +0300

    Update colors.dart

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -4,5 +4,6 @@ class ColorsManager {
    static const Color Primary = Color(0xff2b4e64);
   static const Color red = Color (0xffb7241e);
   static const Color black = Colors.black;
-  
+  static const Color white = Colors.white;
+
 }
